0add2681bbccbd75e9bcb325ef5368aee8dd7ef4
Akhilesh Kumar <akhileshkumarbst0@gmail.com>
2024-02-21 19:04:44 +0530

    react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+function App() {
+  return (
+    <>
+      <Chai/>
+      <h1>Hello Chai Aur React | React ek Javascript 
+      library hai jo ki single page apllication user 
+      interface banae me madad karti hai.</h1>
+    </>
+
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+
+function App() {
+  return (
+    <>
+    <h1>Chai Aur React | Akhilesh Kumar</h1>
+    <Chai/>
+    <p>Test Para</p>
+    </>
+  )
+}
+
+export default App;
