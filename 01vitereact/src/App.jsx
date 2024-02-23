afbd5482efe823c05e9c51e9304d82ec0b666f9d
Akhilesh Kumar <akhileshkumarbst0@gmail.com>
2024-02-23 18:42:58 +0530

    react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,16 @@
 import Chai from "./chai"
 
+
 function App() {
+  const username = "chai aur code"
+
   return (
     <>
-    <h1>Chai Aur React | Akhilesh Kumar</h1>
     <Chai/>
-    <p>Test Para</p>
-    </>
+    <h1>chai aur react {username}</h1>
+    <p>test para</p>
+   </>
   )
 }
 
-export default App;
+export default App
