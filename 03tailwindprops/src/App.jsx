66f4a52f1538b8b20951c0b39f22e9055a13809a
Akhilesh Kumar <akhileshkumarbst0@gmail.com>
2024-02-23 20:24:30 +0530

    react

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -6,18 +6,19 @@ import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
-  let myObj = {
-    username: "Akhilesh",
-    age: 20,
-    email: "akhil@google.com"
-  }
 
-  let newArr= [1,2,3]
+  // let myObj = {
+  //   username: "Akhilesh",
+  //   age: 20,
+  //   email: "akhil@google.com"
+  // }
+
+  // let newArr= [1,2,3]
 
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
-      <Card username = "chai aur code" btnText = "click me"/*somObj = {myObj} someArr ={newArr}*//>
+      <Card username = "chai aur code" btnText = "click me"/*somObj = {myObj} someArr ={newArr} ---> this comment is use to add properties in the card which shown in the in console of browser*/ /> 
       <Card username = " Ankita" btnText = "visit me"/>
     </>
   )
