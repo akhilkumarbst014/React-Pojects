f1aee9e213becb566b395ffd846a1bb223b755e2
Akhilesh Kumar <akhileshkumarbst0@gmail.com>
2024-02-26 22:19:01 +0530

    commit

---
+++ 06currencyConverter/src/App.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-3xl bg-orange-500'>Currency App</h1>
+    </>
+  )
+}
+
+export default App
